4431869c1fd309ce8b6164c65d548b2b975e9df4
Calcium1000 <cehh35@gmail.com>
2025-04-30 22:21:42 +0200

    Added hand anims

--- Assets/RadioBehaviour.cs
+++ Assets/RadioBehaviour.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class RadioBehaviour : MonoBehaviour
 {
+    [SerializeField] AudioSource musicPlayer;
+    public AudioClip technoTrack;
+    public AudioClip classical;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
