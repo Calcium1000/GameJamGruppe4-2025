05113dabedea0f44530b96c7e9bc7d86e741becc
NPK0302 <111127064+NPK0302@users.noreply.github.com>
2025-05-01 13:42:33 +0200

    Update

---
+++ Assets/Scripts/RadioBehaviour.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class RadioBehaviour : MonoBehaviour
+{
+    [SerializeField] AudioSource musicPlayer;
+    public AudioClip technoTrack;
+    public AudioClip classical;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        musicPlayer = GetComponent<AudioSource>();
+        musicPlayer.clip = technoTrack;
+        musicPlayer.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
