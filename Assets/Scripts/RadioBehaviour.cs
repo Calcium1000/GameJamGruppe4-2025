e1c49132e42e0d9ee850a1e1bcec65f796164ece
Purrplexe <piow375@gmail.com>
2025-05-01 22:33:03 +0200

    made player thinner and moved audiosource for radio to radio

--- Assets/Scripts/RadioBehaviour.cs
+++ Assets/Scripts/RadioBehaviour.cs
@@ -2,14 +2,13 @@ using UnityEngine;
 
 public class RadioBehaviour : MonoBehaviour
 {
-    [SerializeField] AudioSource musicPlayer;
+    public AudioSource musicPlayer;
     public AudioClip technoTrack;
     public AudioClip classical;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        musicPlayer = GetComponent<AudioSource>();
         musicPlayer.clip = technoTrack;
         musicPlayer.Play();
     }
